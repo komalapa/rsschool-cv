5b353d39872f7485f1f24f3f8a70acde140418f8
komalapa <komalapa@gmail.com>
2021-09-14 16:08:24 +0400

    feat: adaptivity for footer, columns, projects slider. scroll-x for code examples

--- index.js
+++ index.js
@@ -2,17 +2,30 @@ const buttonRight = document.querySelector('.slider-arrow-right');
 const buttonLeft = document.querySelector('.slider-arrow-left');
 
 const slider = document.querySelector('.projects-slider');
-const maxScroll = slider.scrollWidth - slider.clientWidth;
+let maxScrollWidth = slider.scrollWidth - slider.clientWidth;
+let maxScrollHeight = slider.scrollHeight - slider.clientHeight;
+
 let sliderInterval;
 
 function scrollSlider(step = 10){
-    slider.scrollLeft += step;
-        if (slider.scrollLeft <= 0){
+    let maxScroll = maxScrollWidth;
+    let sliderScroll;
+    if (window.matchMedia("(min-width: 1000px)").matches) {
+        slider.scrollLeft += step;
+        sliderScroll = slider.scrollLeft;
+        //console.log('1',sliderScroll, maxScroll)
+      } else {
+        slider.scrollTop += step;
+        maxScroll = maxScrollHeight;
+        sliderScroll = slider.scrollTop;
+        //console.log('2',sliderScroll,maxScroll)
+      }
+        if (sliderScroll <= 0){
             buttonLeft.classList.add('disabled-arrow')
         } else {
             buttonLeft.classList.remove('disabled-arrow')
         }
-        if (slider.scrollLeft >= maxScroll){
+        if (sliderScroll >= maxScroll){
             buttonRight.classList.add('disabled-arrow')
         } else {
             buttonRight.classList.remove('disabled-arrow')
@@ -38,3 +51,11 @@ buttonLeft.addEventListener('mouseleave',()=>clearInterval(sliderInterval))
 document.addEventListener('mouseup', ()=>clearInterval(sliderInterval))
 
 scrollSlider(0); //init slider position
+
+window.addEventListener('resize', ()=>{ //reinit slider on window resize
+    maxScrollWidth = slider.scrollWidth - slider.clientWidth;
+    maxScrollHeight = slider.scrollHeight- slider.clientHeight;; 
+    slider.scrollLeft = 0;
+    slider.scrollTop = 0;
+    scrollSlider(0)
+})
